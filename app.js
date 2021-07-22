13528466429795c7b4b25257dc80d3a2b56f0826
Deepak Kumar <deepak.kumar.mec20@itbhu.ac.in>
2021-07-22 18:24:27 +0530

    Secondary commit

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require('fs');
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Olearncontact', { useNewUrlParser: true});
 const port = 80;
+
+//Define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    message: String,
+});
+
+var contact = mongoose.model('contacts', contactSchema);
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')); //For serving static files.
@@ -15,12 +29,22 @@ app.set('views', path.join(__dirname, 'views')); //set the view directory
 //ENDPOINTS
 app.get('/', (req, res) => {
     const params = {}
-    res.status(200).render('home.pug', params);
+    res.status(200).render('home.pug',params);
 });
 
 app.get('/contact', (req, res) => {
     const params = {}
-    res.status(200).render('contact.pug', params);
+    res.status(200).render('contact.pug',params);
+});
+
+app.post('/contact', (req, res) => {
+    var myData = new contact(res.body);
+    myData.save().then(() => {
+        res.send("This item has been saved to the database.")
+    }).catch(() => {
+        res.status(404).send("Item was not saved to the database.")
+    })
+    // res.status(200).render('contact.pug');
 });
 
 //START THE SERVER
